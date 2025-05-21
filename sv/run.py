a9ec9b630c0ccc9ae83ccf636b292d7f920e99aa
Tomoki Sugiura <sugiura@info.mie-u.ac.jp>
2025-05-21 13:57:31 +0900

    fix run script

--- sv/run.py
+++ sv/run.py
@@ -1,6 +1,7 @@
 from vunit.verilog import VUnit
 
-vu = VUnit.from_argv()
+vu = VUnit.from_argv(compile_builtins=False)
+vu.add_verilog_builtins()
 lib = vu.add_library("lib")
 lib.add_source_files("src/*.sv")
 lib.add_source_files("tb/*.sv")
